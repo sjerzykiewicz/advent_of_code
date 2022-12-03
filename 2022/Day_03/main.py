22e9353719c954d1fc40d425ea29d173e5f4a653
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2022-12-03 12:55:04 +0100

    Day_03

---
+++ 2022/Day_03/main.py
@@ -0,0 +1,42 @@
+from re import L
+
+
+def main():
+    lines = []
+
+    with open("input.txt", "r") as f:
+        for line in f:
+            lines.append(line.strip("\n"))
+
+    result = []
+
+    # Part 1
+    sum = 0
+    for line in lines:
+        r_1 = line[: len(line) // 2]
+        r_2 = line[len(line) // 2 :]
+        common_char = "".join(set(r_1).intersection(r_2))
+        sum += calc_value(common_char)
+    result.append(sum)
+
+    # Part 2s
+    sum = 0
+    for i in range(0, len(lines) - 2, 3):
+        common_char = "".join(set(lines[i]) & set(lines[i + 1]) & set(lines[i + 2]))
+        sum += calc_value(common_char)
+    result.append(sum)
+
+    with open("output.txt", "w") as f:
+        for i in result:
+            f.write(f"{i}\n")
+
+
+def calc_value(char):
+    if char.islower():
+        return ord(char) - 96
+    else:
+        return ord(char) - 38
+
+
+if __name__ == "__main__":
+    main()
