d36a1e5e86faff90a1a8ef147b180cb8abf18761
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2022-12-04 11:13:41 +0100

    Day_01 in GO

---
+++ 2022/Day_01/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+	"io"
+	"strconv"
+	"sort"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+	calories := []int{0}
+	i := 0
+    for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		lineStr := string(line[:])
+        if len(line) == 0 {
+			i++
+			calories = append(calories, 0)
+		} else {
+			calorie, _ := strconv.Atoi(lineStr)
+			calories[i] += calorie
+		}
+    }
+
+	sort.Ints(calories)
+
+	// Part 1
+	fmt.Printf("%v\n", calories[len(calories)-1])
+
+	// Part 2
+	sum := calories[len(calories)-1] + calories[len(calories)-2] + calories[len(calories)-3]
+	fmt.Printf("%v\n", sum)
+}
