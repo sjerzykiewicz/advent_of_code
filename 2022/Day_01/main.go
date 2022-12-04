b03b28f6253006c05e192dc6def6958280ae092d
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2022-12-04 15:41:40 +0100

    Day_01 in go

--- 2022/Day_01/main.go
+++ 2022/Day_01/main.go
@@ -5,43 +5,43 @@ import (
     "fmt"
     "log"
     "os"
-	"io"
-	"strconv"
-	"sort"
+    "io"
+    "strconv"
+    "sort"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+    file, err := os.Open("input.txt")
     if err != nil {
         log.Fatal(err)
     }
     defer file.Close()
 
     reader := bufio.NewReader(file)
-	calories := []int{0}
-	i := 0
+    calories := []int{0}
+    i := 0
     for {
-		line, _, err := reader.ReadLine()
-		if err == io.EOF {
-			break
-		}
+        line, _, err := reader.ReadLine()
+        if err == io.EOF {
+            break
+        }
 
-		lineStr := string(line[:])
+        lineStr := string(line[:])
         if len(line) == 0 {
-			i++
-			calories = append(calories, 0)
-		} else {
-			calorie, _ := strconv.Atoi(lineStr)
-			calories[i] += calorie
-		}
+            i++
+            calories = append(calories, 0)
+        } else {
+            calorie, _ := strconv.Atoi(lineStr)
+            calories[i] += calorie
+        }
     }
 
-	sort.Ints(calories)
+    sort.Ints(calories)
 
-	// Part 1
-	fmt.Printf("%v\n", calories[len(calories)-1])
+    // Part 1
+    fmt.Printf("%v\n", calories[len(calories)-1])
 
-	// Part 2
-	sum := calories[len(calories)-1] + calories[len(calories)-2] + calories[len(calories)-3]
-	fmt.Printf("%v\n", sum)
+    // Part 2
+    sum := calories[len(calories)-1] + calories[len(calories)-2] + calories[len(calories)-3]
+    fmt.Printf("%v\n", sum)
 }
