224d9ea6fbfbe927caea84cbddb2c7626fb14533
sjerzykiewicz <sebastian.jerzykiewicz@gmail.com>
2024-03-22 15:24:23 +0100

    day_01 done

--- 2015/day_01/main.hs
+++ 2015/day_01/main.hs
@@ -4,18 +4,18 @@ main :: IO ()
 main = do
   fileHandle <- openFile "input.txt" ReadMode
   contents <- hGetContents fileHandle
-  putStrLn ("Part One: " ++ show (find_floor contents))
-  putStrLn ("Part Two: " ++ show (find_basement contents 0 1))
+  putStrLn ("Part One: " ++ show (findFloor contents))
+  putStrLn ("Part Two: " ++ show (findBasement contents 0 1))
   hClose fileHandle
 
-find_floor :: [Char] -> Int
-find_floor [] = 0
-find_floor ('(':xs) = find_floor xs + 1
-find_floor (')':xs) = find_floor xs - 1
+findFloor :: [Char] -> Int
+findFloor [] = 0
+findFloor ('(':xs) = findFloor xs + 1
+findFloor (')':xs) = findFloor xs - 1
 
-find_basement :: [Char] -> Int -> Int -> Int
-find_basement [] _ _ = 0
-find_basement ('(':xs) floor position = find_basement xs (floor + 1) (position + 1)
-find_basement (')':xs) floor position
+findBasement :: [Char] -> Int -> Int -> Int
+findBasement [] _ _ = 0
+findBasement ('(':xs) floor position = findBasement xs (floor + 1) (position + 1)
+findBasement (')':xs) floor position
   | floor == 0 = position
-  | otherwise = find_basement xs (floor - 1) (position + 1)
+  | otherwise = findBasement xs (floor - 1) (position + 1)
